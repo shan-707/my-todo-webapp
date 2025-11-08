a429d25b9949598a90e5d2b088674eabb6db61af
shant <shantanu0116@gmail.com>
2025-11-08 12:53:09 +0530

    Added changes to functions.py for deployment purposes

--- functions.py
+++ functions.py
@@ -1,4 +1,9 @@
+import os
+
 FILEPATH = "C:\Shan\python\To_Do_Web\Tasks_file.txt"
+
+# Get the absolute path to this folder
+FILEPATH = os.path.join(os.path.dirname(__file__), "Tasks_file.txt")
 
 def get_todos(filepath=FILEPATH):
     with open(filepath, 'r') as fh:
